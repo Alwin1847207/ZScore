8ae91d342dda62ae3b5d1c94df67a2a69bc90371
ALWIN JOSEPH <47514997+Alwin1847207@users.noreply.github.com>
2019-12-09 13:00:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="alwin_1997", # Replace with your own username
-    version="0.0.1",
-    author="ALWIN JOSEPH",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+from distutils.core import setup
+setup(
+  name = 'ZScore',         # How you named your package folder (MyLib)
+  packages = ['ZScore'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Performs ZScore normalization in your dataset.',   # Give a short description about your library
+  author = 'ALWIN JOSEPH',                   # Type in your name
+  author_email = 'alwin.joseph@mca.christuniversity.in',      # Type in your E-Mail
+  url = 'https://github.com/Alwin1847207/ZScore.git',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Alwin1847207/ZScore/archive/v0.1.tar.gz',    # I explain this later on
+  keywords = ['ZScore','Z Score','Z-Score', 'normalization', 'python'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'pandas',
+          'numpy',
+      'statistics',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
