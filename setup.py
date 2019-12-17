d92acbd4aa4080694e8c2900566b1f9343ce4bf3
ALWIN JOSEPH <47514997+Alwin1847207@users.noreply.github.com>
2019-12-17 16:25:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'ALWIN JOSEPH',                   # Type in your name
   author_email = 'alwin.joseph@mca.christuniversity.in',      # Type in your E-Mail
   url = 'https://github.com/Alwin1847207/zscore.git',   # Provide either the link to your github or to your website
-  download_url = '',    # I explain this later on
+  download_url = 'https://github.com/Alwin1847207/zscore/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['zscore','z score','z-score','ZScore','Z Score','Z-Score', 'normalization', 'python'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
